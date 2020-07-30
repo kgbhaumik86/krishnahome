2dca3c84a30ba9bb3762265cd2288ceb2a68da9b
kgbhaumik86 <kgbhaumik86@gmail.com>
2020-07-30 13:38:06 +0530

    Updates

---
+++ precache-manifest.61def4dbaf61c99d6c21c6edb6a18938.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2a0e39e0bba2dfb430f921f33290de2",
+    "url": "/krishnahome/index.html"
+  },
+  {
+    "revision": "0db09f23b63162daa172",
+    "url": "/krishnahome/static/css/2.b4a1cdfd.chunk.css"
+  },
+  {
+    "revision": "01281d5a85de173957e9",
+    "url": "/krishnahome/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0db09f23b63162daa172",
+    "url": "/krishnahome/static/js/2.2ccc6ab8.chunk.js"
+  },
+  {
+    "revision": "94b7903c1eab122241d78a9be26443c5",
+    "url": "/krishnahome/static/js/2.2ccc6ab8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01281d5a85de173957e9",
+    "url": "/krishnahome/static/js/main.daa49c03.chunk.js"
+  },
+  {
+    "revision": "b40a5c3d6f052baf4c37",
+    "url": "/krishnahome/static/js/runtime-main.0f05f256.js"
+  },
+  {
+    "revision": "38f760e105df1b822fe5eb923ef31209",
+    "url": "/krishnahome/static/media/KgbResume.38f760e1.pdf"
+  }
+]);
